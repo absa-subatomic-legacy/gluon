1ec6474318ae1d504a59e9f32263f7d09bfdf739
kbristow <Kieran.Bristow@absa.co.za>
2018-02-12 10:06:26 +0200

    -Reformatted using the eclipse code formatter template

--- nucleus/src/main/java/za/co/absa/subatomic/infrastructure/AtomistConfiguration.java
+++ nucleus/src/main/java/za/co/absa/subatomic/infrastructure/AtomistConfiguration.java
@@ -1,21 +1,30 @@
 package za.co.absa.subatomic.infrastructure;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
 
 @Data
-@Configuration
 @ConfigurationProperties("atomist")
 public class AtomistConfiguration {
+
     private String slackIdentityUrl;
+
     private String teamUrl;
+
     private String teamCreatedEventUrl;
+
     private String teamMemberCreatedEventUrl;
+
     private String projectCreatedEventUrl;
+
     private String bitbucketProjectRequestedEventUrl;
+
     private String bitbucketProjectAddedEventUrl;
+
     private String devOpsEnvironmentRequestedEventUrl;
+
     private String projectEnvironmentsRequestedEventUrl;
+
     private String applicationCreatedEventUrl;
 }
