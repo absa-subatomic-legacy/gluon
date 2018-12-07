e225dabdcc5a6f00837240c4055ed72a119b688c
kbristow <Kieran.Bristow@absa.co.za>
2018-12-07 16:16:41 +0200

    Removed unused axon classes

--- nucleus/src/main/java/za/co/absa/subatomic/domain/project/ProjectDeleted.java
+++
@@ -1,9 +0,0 @@
-package za.co.absa.subatomic.domain.project;
-
-import lombok.Value;
-import za.co.absa.subatomic.domain.pkg.ProjectId;
-
-@Value
-public class ProjectDeleted {
-    private ProjectId projectId;
-}
