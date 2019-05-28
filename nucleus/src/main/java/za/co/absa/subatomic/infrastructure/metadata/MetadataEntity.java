6bfb659cc126d2dfacd0e47a5984cc36afb95f2e
Subatomic <subatomic@local>
2019-05-28 10:05:02 +0200

    Initial trial implementation and testing

---
+++ nucleus/src/main/java/za/co/absa/subatomic/infrastructure/metadata/MetadataEntity.java
@@ -0,0 +1,23 @@
+package za.co.absa.subatomic.infrastructure.metadata;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "metadata")
+@Setter(value = AccessLevel.PACKAGE)
+@Getter
+public class MetadataEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+
+    @OneToMany
+    private List<MetadataEntry> metadataEntries;
+}
---
+++ nucleus/src/main/java/za/co/absa/subatomic/infrastructure/metadata/MetadataEntry.java
@@ -0,0 +1,23 @@
+package za.co.absa.subatomic.infrastructure.metadata;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "metadataEntries")
+@Setter(value = AccessLevel.PACKAGE)
+@Getter
+public class MetadataEntry {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String key;
+    private String value;
+}
